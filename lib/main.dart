51daa6f6cf670206c5a14f5026778d9f69e22bbc
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2023-12-17 10:38:37 +0530

    DONE: flare animation in rating emojis

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:f_animetion/rive_flr_animation/emojis_rating.dart';
 import 'package:flutter/material.dart';
-import 'Drawing/clipove.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ClipWidgets(),
+      home: EmojisRating(title: 'Flutter Animation Demo.'),
     );
   }
 }
