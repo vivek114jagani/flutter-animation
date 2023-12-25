a62c3389a7dd98acf278a18d3d21af32ba0c5a05
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2023-12-25 11:36:57 +0530

    DONE: animation application project setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:f_animetion/rive_flr_animation/emojis_rating.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: EmojisRating(title: 'Flutter Animation Demo.'),
+      // home: ,
     );
   }
 }
